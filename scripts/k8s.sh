ac8b02fd99bc1da7fcda1c4e93d2b387f3b63ca9
Evan Bee <evan2884@gmail.com>
2025-03-18 23:49:42 +0800

    feat(scripts): 添加 k8s.sh 脚本用于拉取 Kubernetes 镜像
    - 新增 k8s.sh 脚本文件
    - 从阿里云镜像仓库拉取 Kubernetes 相关镜像
    - 将拉取的镜像重新打标签为 k8s.gcr.io前缀
    - 删除原始标签的镜像
    - 显示所需镜像和已安装镜像

---
+++ scripts/k8s.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#echo "修改docker普通用户权限"
+#sudo chmod 777 /var/run/docker.sock
+
+echo "1.拉取镜像"
+
+#下面的版本号要对应
+KUBE_VERSION=v1.19.16
+PAUSE_VERSION=3.2
+CORE_DNS_VERSION=1.7.0
+ETCD_VERSION=3.4.13-0
+
+# pull kubernetes images from hub.docker.com
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION
+# pull aliyuncs mirror docker images
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION
+
+# retag to k8s.gcr.io prefix
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION  k8s.gcr.io/kube-proxy:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION k8s.gcr.io/kube-controller-manager:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION k8s.gcr.io/kube-apiserver:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION k8s.gcr.io/kube-scheduler:$KUBE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION k8s.gcr.io/pause:$PAUSE_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION k8s.gcr.io/coredns:$CORE_DNS_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION k8s.gcr.io/etcd:$ETCD_VERSION
+
+# untag origin tag, the images won't be delete.
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:$KUBE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/pause:$PAUSE_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$CORE_DNS_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:$ETCD_VERSION
+
+echo "====================执行完毕======================"
+
+echo "所需镜像:"
+kubeadm config images list
+
+echo "已安装镜像:"
+docker images
+
+echo "====如果数量不匹配请多执行几次k8s_pull_images.sh====="
